63ebd906ce43af089115d1c513b68736bb344e12
Bilal <mb336450@gmail.com>
2025-11-06 17:22:17 +0700

    setup pusher js

--- resources/views/dashboard/layouts/tail.blade.php
+++ resources/views/dashboard/layouts/tail.blade.php
@@ -1,15 +1,43 @@
-    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <audio id="alert-sound" src="{{ asset('assets/sound/alert.mp3') }}" preload="auto"></audio>
 
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://js.pusher.com/8.2/pusher.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            const audio = document.getElementById('alert-sound');
+            $(document).one('click', function () {
+                audio.play().then(() => {
+                    audio.pause();
+                    audio.currentTime = 0;
+                }).catch(() => {});
+                console.log('🔊 Audio unlocked by user interaction');
+            });
+
+            Pusher.logToConsole = false;
+            const pusher = new Pusher("{{ env('PUSHER_APP_KEY') }}", {
+                cluster: "{{ env('PUSHER_APP_CLUSTER') }}",
+                forceTLS: true
+            });
+
+            const mountainId = "{{ auth()->user()->mountain_id }}";
+            const channel = pusher.subscribe(`mountain-sos.${mountainId}`);
+
+            channel.bind('sos.created', function (data) {
+                console.log('📡 SOS event received:', data);
+                audio.play().catch(err => console.warn('Audio play blocked:', err));
+                alert(`🚨 SOS DARI PENDAKI!\nPesan: ${data.sosData.message}\nKoordinat: ${data.sosData.latitude}, ${data.sosData.longitude}`);
+            });
+        });
+    </script>
+
+    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
     <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/node-waves/node-waves.js') }}"></script>
-
     <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-
     <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
-
     <!-- endbuild -->
-
     <!-- Vendors JS -->
     <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
 
